1b213f87236e84b8509ad704b057be8c00158e91
joshuacfowler <jcf3@rice.edu>
2019-11-23 19:37:11 -0600

    calc means of parameters for matrix. the bigmatrix function still isn't working because of the trunc. neg binom. growth model

--- endodemog_matrix.R
+++ endodemog_matrix.R
@@ -12,55 +12,64 @@ library(bbmle)
 invlogit<-function(x){exp(x)/(1+exp(x))}
 logit = function(x) { log(x/(1-x)) }
 
+#############################################################################################
+####### Data manipulation to prepare data as lists for Stan models------------------
+#############################################################################################
+
+# Growth data lists are generated in the endodemog_data_processing.R file
+# within the section titled "Preparing datalists for Growth Kernel"
+source("endodemog_data_processing.R")
+
 
 #  Read in the model outputs ----------------------------------------------
 source("endodemog_model_outputs.R")
 
-
+#############################################################################################
 # Assembling matrix model -------------------------------------------------
+#############################################################################################
 
 # LOAR
 # collect all the parameters into one vector
 # collect the survival parameters
 loar_params <- c()
-loar_params[1] <- rstan::extract(survLOAR, pars = "beta[1]"); names(loar_params)[1] <- "surv_beta1"
-loar_params[2] <- rstan::extract(survLOAR, pars = "beta[2]"); names(loar_params)[2] <- "surv_beta2"
-loar_params[3] <- rstan::extract(survLOAR, pars = "beta[3]"); names(loar_params)[3] <- "surv_beta3"
-loar_params[4] <- rstan::extract(survLOAR, pars = "beta[4]"); names(loar_params)[4] <- "surv_beta4"
+loar_params[1] <- lapply(rstan::extract(survLOAR, pars = "beta[1]"), FUN = mean); names(loar_params)[1] <- "surv_beta1"
+loar_params[2] <- lapply(rstan::extract(survLOAR, pars = "beta[2]"), FUN = mean); names(loar_params)[2] <- "surv_beta2"
+loar_params[3] <- lapply(rstan::extract(survLOAR, pars = "beta[3]"), FUN = mean); names(loar_params)[3] <- "surv_beta3"
+loar_params[4] <- lapply(rstan::extract(survLOAR, pars = "beta[4]"), FUN = mean); names(loar_params)[4] <- "surv_beta4"
 # collect the growth parameters
-loar_params[5] <- rstan::extract(growLOAR, pars = "beta[1]"); names(loar_params)[5] <- "grow_beta1"
-loar_params[6] <- rstan::extract(growLOAR, pars = "beta[2]"); names(loar_params)[6] <- "grow_beta2"
-loar_params[7] <- rstan::extract(growLOAR, pars = "beta[3]"); names(loar_params)[7] <- "grow_beta3"
-loar_params[8] <- rstan::extract(growLOAR, pars = "beta[4]"); names(loar_params)[8] <- "grow_beta4"
+loar_params[5] <- lapply(rstan::extract(growLOAR, pars = "beta[1]"), FUN = mean); names(loar_params)[5] <- "grow_beta1"
+loar_params[6] <- lapply(rstan::extract(growLOAR, pars = "beta[2]"), FUN = mean); names(loar_params)[6] <- "grow_beta2"
+loar_params[7] <- lapply(rstan::extract(growLOAR, pars = "beta[3]"), FUN = mean); names(loar_params)[7] <- "grow_beta3"
+loar_params[8] <- lapply(rstan::extract(growLOAR, pars = "beta[4]"), FUN = mean); names(loar_params)[8] <- "grow_beta4"
 # collect the flowering parameters
-loar_params[9] <- rstan::extract(flwLOAR, pars = "beta[1]"); names(loar_params)[9] <- "flw_beta1"
-loar_params[10] <- rstan::extract(flwLOAR, pars = "beta[2]"); names(loar_params)[10] <- "flw_beta2"
-loar_params[11] <- rstan::extract(flwLOAR, pars = "beta[3]"); names(loar_params)[11] <- "flw_beta3"
-loar_params[12] <- rstan::extract(flwLOAR, pars = "beta[4]"); names(loar_params)[12] <- "flw_beta4"
+loar_params[9] <- lapply(rstan::extract(flwLOAR, pars = "beta[1]"), FUN = mean); names(loar_params)[9] <- "flw_beta1"
+loar_params[10] <- lapply(rstan::extract(flwLOAR, pars = "beta[2]"), FUN = mean); names(loar_params)[10] <- "flw_beta2"
+loar_params[11] <- lapply(rstan::extract(flwLOAR, pars = "beta[3]"), FUN = mean); names(loar_params)[11] <- "flw_beta3"
+loar_params[12] <- lapply(rstan::extract(flwLOAR, pars = "beta[4]"), FUN = mean); names(loar_params)[12] <- "flw_beta4"
 # collect the fertility parameters
-loar_params[13] <- rstan::extract(fertLOAR, pars = "beta[1]"); names(loar_params)[13] <- "fert_beta1"
-loar_params[14] <- rstan::extract(fertLOAR, pars = "beta[2]"); names(loar_params)[14] <- "fert_beta2"
-loar_params[15] <- rstan::extract(fertLOAR, pars = "beta[3]"); names(loar_params)[15] <- "fert_beta3"
-loar_params[16] <- rstan::extract(fertLOAR, pars = "beta[4]"); names(loar_params)[16] <- "fert_beta4"
+loar_params[13] <- lapply(rstan::extract(fertLOAR, pars = "beta[1]"), FUN = mean); names(loar_params)[13] <- "fert_beta1"
+loar_params[14] <- lapply(rstan::extract(fertLOAR, pars = "beta[2]"), FUN = mean); names(loar_params)[14] <- "fert_beta2"
+loar_params[15] <- lapply(rstan::extract(fertLOAR, pars = "beta[3]"), FUN = mean); names(loar_params)[15] <- "fert_beta3"
+loar_params[16] <- lapply(rstan::extract(fertLOAR, pars = "beta[4]"), FUN = mean); names(loar_params)[16] <- "fert_beta4"
 # collect the spikelets/infl parameters
-loar_params[17] <- rstan::extract(spikeLOAR, pars = "beta[1]"); names(loar_params)[17] <- "spike_beta1"
-loar_params[18] <- rstan::extract(spikeLOAR, pars = "beta[2]"); names(loar_params)[18] <- "spike_beta2"
-loar_params[19] <- rstan::extract(spikeLOAR, pars = "beta[3]"); names(loar_params)[19] <- "spike_beta3"
-loar_params[20] <- rstan::extract(spikeLOAR, pars = "beta[4]"); names(loar_params)[20] <- "spike_beta4"
+loar_params[17] <- lapply(rstan::extract(spikeLOAR, pars = "beta[1]"), FUN = mean); names(loar_params)[17] <- "spike_beta1"
+loar_params[18] <- lapply(rstan::extract(spikeLOAR, pars = "beta[2]"), FUN = mean); names(loar_params)[18] <- "spike_beta2"
+loar_params[19] <- lapply(rstan::extract(spikeLOAR, pars = "beta[3]"), FUN = mean); names(loar_params)[19] <- "spike_beta3"
+loar_params[20] <- lapply(rstan::extract(spikeLOAR, pars = "beta[4]"), FUN = mean); names(loar_params)[20] <- "spike_beta4"
 # collect the seed/spikelet parameters
-loar_params[21] <- rstan::extract(seedLOAR, pars = "mu_seed"); names(loar_params)[21] <- "mu_seed"
+loar_params[21] <- lapply(rstan::extract(seedLOAR, pars = "mu_seed"), FUN = mean); names(loar_params)[21] <- "mu_seed"
 # collect the recruitment parameters
-loar_params[22] <- rstan::extract(s_to_s_LOAR, pars = "beta[1]"); names(loar_params)[22] <- "s_to_s_beta1"
-loar_params[23] <- rstan::extract(s_to_s_LOAR, pars = "beta[2]"); names(loar_params)[23] <- "s_to_s_beta2"
+loar_params[22] <- lapply(rstan::extract(s_to_sLOAR, pars = "beta[1]"), FUN = mean); names(loar_params)[22] <- "s_to_s_beta1"
+loar_params[23] <- lapply(rstan::extract(s_to_sLOAR, pars = "beta[2]"), FUN = mean); names(loar_params)[23] <- "s_to_s_beta2"
 # min and max size
 loar_params[24] <- 1; names(loar_params)[24]<-"min_size"
 loar_params[25] <- quantile(LOAR_grow_data_list$size_t1,0.95,na.rm=T); names(loar_params)[25]<-"max_size"
 # note that I define max size as the 95TH pctile of observed sized. The very max sizes observed often have very poor 
 # replication, and I find that these few observations (and the corresponding vital rate predictions) can have a strong
 # influence on the results. So this approach is conservative, but you can always experiment with this.
-loar_params[26] <- rstan::extract(growLOAR, pars = "phi"); names(loar_params[26]) <- "grow_phi"
-loar_params[27] <- rstan::extract(fertLOAR, pars = "phi"); names(loar_params[27]) <- "fert_phi"
-loar_params[28] <- rstan::extract(spikeLOAR, pars = "phi"); names(loar_params[28]) <- "spike_phi"
+loar_params[26] <- lapply(rstan::extract(growLOAR, pars = "phi"), FUN = mean); names(loar_params[26]) <- "grow_phi"
+loar_params[27] <- lapply(rstan::extract(fertLOAR, pars = "phi"), FUN = mean); names(loar_params[27]) <- "fert_phi"
+loar_params[28] <- lapply(rstan::extract(spikeLOAR, pars = "phi"), FUN = mean); names(loar_params[28]) <- "spike_phi"
 
 
 # define functions that will be used to populate projection matrix
@@ -100,8 +109,8 @@ fx<-function(x,params){
 # finally, here is the function that takes the parameter vector and assembles the matrix model from all of the pieces
 bigmatrix<-function(params){   
   
-  matdim<-params$max_size ## matrix dimension
-  y<-1:params$max_size ## size (tiller number) associated with each class
+  matdim<-as.numeric(params["max_size"])## matrix dimension
+  y<-1:as.numeric(params["max_size"])## size (tiller number) associated with each class
   
   # Fertility matrix
   Fmat<-matrix(0,matdim,matdim)
