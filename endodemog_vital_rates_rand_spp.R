407342a2d71e5704928a36fb377d6e4905951c8c
unknown <tom.miller@rice.edu>
2019-12-11 21:31:20 -0500

    Ready to run the survival model (no endo effect on variance yet)

--- endodemog_vital_rates_rand_spp.R
+++ endodemog_vital_rates_rand_spp.R
@@ -1,8 +1,11 @@
 library(tidyverse)
+library(rstan)
+rstan_options( auto_write = TRUE )
+options( mc.cores = parallel::detectCores() )
 
 joshpath <- "/Users/joshuacfowler/Dropbox/EndodemogData/"
-#tompath <- "C:/Users/tm9/Dropbox/EndodemogData/"
-tompath <- "C:/Users/tm634/Dropbox/EndodemogData/"
+tompath <- "C:/Users/tm9/Dropbox/EndodemogData/"
+#tompath <- "C:/Users/tm634/Dropbox/EndodemogData/"
 
 LTREB_full <- read_csv(paste0(tompath,"Fulldataplusmetadata/LTREB_full.csv"))
 LTREB_data_forsurv <- LTREB_full %>%
@@ -10,3 +13,40 @@ LTREB_data_forsurv <- LTREB_full %>%
   filter(!is.na(logsize_t)) %>% 
   filter(logsize_t >= 0) %>% 
   filter(!is.na(endo_01))
+
+## this is good, there are no plot numbers duplicated across species
+table(LTREB_data_forsurv$plot_fixed,LTREB_data_forsurv$species)
+
+## note that this is how species are indexing:
+cbind(unique(LTREB_data_forsurv$species),
+as.integer(as.numeric(as.factor(unique(LTREB_data_forsurv$species)))))
+
+surv_dat <- list(nYear = unique(LTREB_data_forsurv$year_t - (min(LTREB_data_forsurv$year_t)-1)),
+                 nPlot = max(LTREB_data_forsurv$plot_fixed),
+                 nSpp = length(unique(LTREB_data_forsurv$species)),
+                 N = nrow(LTREB_data_forsurv),
+                 year_t = LTREB_data_forsurv$year_t - (min(LTREB_data_forsurv$year_t)-1),
+                 plot = LTREB_data_forsurv$plot_fixed,
+                 spp = as.integer(as.numeric(as.factor(LTREB_data_forsurv$species))),
+                 y = LTREB_data_forsurv$surv_t1,
+                 logsize_t = LTREB_data_forsurv$logsize_t,
+                 endo_01 = LTREB_data_forsurv$endo_01,
+                 origin_01 = LTREB_data_forsurv$origin_01)
+  
+sim_pars <- list(
+  warmup = 1000, 
+  iter = 10000, 
+  thin = 3, 
+  chains = 2
+)
+
+surv_fit <- stan(
+  file = 'survival_flowering_random_spp.stan',
+  data = surv_dat,
+  warmup = sim_pars$warmup,
+  iter = sim_pars$iter,
+  thin = sim_pars$thin,
+  chains = sim_pars$chains )
+
+
+
