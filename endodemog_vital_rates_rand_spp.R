3f7afa844061c6bcfd38697077ead567412c115c
unknown <tom.miller@rice.edu>
2019-12-12 21:07:10 -0500

    Coded up all the main vital rates and ready to fit.

--- endodemog_vital_rates_rand_spp.R
+++ endodemog_vital_rates_rand_spp.R
@@ -19,6 +19,16 @@ LTREB_data_forflw <- LTREB_full %>%
   filter(!is.na(FLW_STAT_T1)) %>% 
   filter(!is.na(logsize_t1)) %>% 
   filter(!is.na(endo_01))
+LTREB_data_forgrow <- LTREB_full %>%
+  filter(!is.na(logsize_t)) %>% 
+  filter(!is.na(size_t1)) %>% 
+  filter(!is.na(endo_01)) %>% 
+  mutate(size_t1 = as.integer(size_t1))
+## again, here I am using size_t1 and flw_count_t1
+LTREB_data_forfert <- LTREB_full %>% 
+  filter(!is.na(FLW_COUNT_T1)) %>% 
+  filter(FLW_COUNT_T1 > 0) %>% 
+  filter(!is.na(logsize_t1))
 
 ## this is good, there are no plot numbers duplicated across species
 table(LTREB_data_forsurv$plot_fixed,LTREB_data_forsurv$species)
@@ -39,7 +49,6 @@ surv_dat <- list(nYear = length(unique(LTREB_data_forsurv$year_t - (min(LTREB_da
                  logsize_t = LTREB_data_forsurv$logsize_t,
                  endo_01 = LTREB_data_forsurv$endo_01,
                  origin_01 = LTREB_data_forsurv$origin_01)
-
 flow_dat <- list(nYear = length(unique(LTREB_data_forflw$year_t - (min(LTREB_data_forflw$year_t)-1))),
                  nPlot = max(LTREB_data_forflw$plot_fixed),
                  nSpp = length(unique(LTREB_data_forflw$species)),
@@ -52,12 +61,36 @@ flow_dat <- list(nYear = length(unique(LTREB_data_forflw$year_t - (min(LTREB_dat
                  logsize_t = LTREB_data_forflw$logsize_t1,
                  endo_01 = LTREB_data_forflw$endo_01,
                  origin_01 = LTREB_data_forflw$origin_01)
-  
+grow_dat <- list(nYear = length(unique(LTREB_data_forgrow$year_t - (min(LTREB_data_forgrow$year_t)-1))),
+                 nPlot = max(LTREB_data_forgrow$plot_fixed),
+                 nSpp = length(unique(LTREB_data_forgrow$species)),
+                 nEndo=length(unique(LTREB_data_forgrow$endo_01)),
+                 N = nrow(LTREB_data_forgrow),
+                 year_t = LTREB_data_forgrow$year_t - (min(LTREB_data_forgrow$year_t)-1),
+                 plot = LTREB_data_forgrow$plot_fixed,
+                 spp = as.integer(as.numeric(as.factor(LTREB_data_forgrow$species))),
+                 y = LTREB_data_forgrow$size_t1,
+                 logsize_t = LTREB_data_forgrow$logsize_t,
+                 endo_01 = LTREB_data_forgrow$endo_01,
+                 origin_01 = LTREB_data_forgrow$origin_01)
+fert_dat <- list(nYear = length(unique(LTREB_data_forfert$year_t - (min(LTREB_data_forfert$year_t)-1))),
+                 nPlot = max(LTREB_data_forfert$plot_fixed),
+                 nSpp = length(unique(LTREB_data_forfert$species)),
+                 nEndo=length(unique(LTREB_data_forfert$endo_01)),
+                 N = nrow(LTREB_data_forfert),
+                 year_t = LTREB_data_forfert$year_t - (min(LTREB_data_forfert$year_t)-1),
+                 plot = LTREB_data_forfert$plot_fixed,
+                 spp = as.integer(as.numeric(as.factor(LTREB_data_forfert$species))),
+                 y = LTREB_data_forfert$FLW_COUNT_T1,
+                 logsize_t = LTREB_data_forfert$logsize_t1,
+                 endo_01 = LTREB_data_forfert$endo_01,
+                 origin_01 = LTREB_data_forfert$origin_01)
+
 sim_pars <- list(
   warmup = 1000, 
-  iter = 10000, 
+  iter = 5000, 
   thin = 3, 
-  chains = 3
+  chains = 1
 )
 
 surv_fit <- stan(
@@ -67,7 +100,10 @@ surv_fit <- stan(
   iter = sim_pars$iter,
   thin = sim_pars$thin,
   chains = sim_pars$chains )
-write_rds(surv_fit,paste0(tompath,"Fulldataplusmetadata/SppRFX/surv_fit.rds"))
+#write_rds(surv_fit,paste0(tompath,"Fulldataplusmetadata/SppRFX/surv_fit.rds"))
+surv_fit <- read_rds(paste0(tompath,"Fulldataplusmetadata/SppRFX/surv_fit.rds"))
+mcmc_dens_overlay(surv_fit,pars = c("mu_betaendo"))
+mcmc_rhat(surv_fit,pars = c("mu_betaendo"))
 
 flow_fit <- stan(
   file = 'survival_flowering_random_spp.stan',
@@ -78,8 +114,23 @@ flow_fit <- stan(
   chains = sim_pars$chains )
 write_rds(flow_fit,paste0(tompath,"Fulldataplusmetadata/SppRFX/flow_fit.rds"))
 
+grow_fit <- stan(
+  file = 'growth_fertility_random_spp.stan',
+  data = grow_dat,
+  warmup = sim_pars$warmup,
+  iter = sim_pars$iter,
+  thin = sim_pars$thin,
+  chains = sim_pars$chains )
+write_rds(grow_fit,paste0(tompath,"Fulldataplusmetadata/SppRFX/grow_fit.rds"))
 
-
+fert_fit <- stan(
+  file = 'growth_fertility_random_spp.stan',
+  data = fert_dat,
+  warmup = sim_pars$warmup,
+  iter = sim_pars$iter,
+  thin = sim_pars$thin,
+  chains = sim_pars$chains )
+write_rds(fert_fit,paste0(tompath,"Fulldataplusmetadata/SppRFX/fert_fit.rds"))
 
 
 mcmc_trace(surv_fit,par=c("beta0"))
